8bbd83d44be1412296bedded6d078d8e8c194db6
Ilya Levin <bskton@gmail.com>
2018-11-26 22:07:52 +1000

    Person entiry and migration

---
+++ app/DoctrineMigrations/Version20181126120413.php
@@ -0,0 +1,30 @@
+<?php declare(strict_types=1);
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20181126120413 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('CREATE SEQUENCE person_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
+        $this->addSql('CREATE TABLE person (id INT NOT NULL, firstName VARCHAR(70) NOT NULL, lastName VARCHAR(100) NOT NULL, dateOfBirth DATE NOT NULL, PRIMARY KEY(id))');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'postgresql', 'Migration can only be executed safely on \'postgresql\'.');
+
+        $this->addSql('DROP SEQUENCE person_id_seq CASCADE');
+        $this->addSql('DROP TABLE person');
+    }
+}
